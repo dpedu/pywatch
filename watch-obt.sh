1472bad71f6fa1aa0f6f6084b5f42f0aec1d75de
dpedu <dave@davepedu.com>
2015-06-25 10:22:16 -0700

    Fix script

--- watch-obt.sh
+++ watch-obt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #source testenv/bin/activate
-./watch2.py ~/PhpstormProjects/pronq/obt_prototypes/ -u root -p vagrant -s localdev.saas.hp.com \
+./watch.py ~/PhpstormProjects/pronq/obt_prototypes/ -u root -p vagrant -s localdev.saas.hp.com \
     --map "server/cms:/var/www/drupal" \
     --map "shared-assets:/var/www/drupal/sites/all/themes/zen_pronq_mktg/shared-assets" \
     --map ":/root"
