4fdcea8acdcf319a4ede6f5941b36d6d697b4aaa
dpedu <dave@davepedu.com>
2015-06-25 10:29:21 -0700

    Auto-create environment

--- watch-obt.sh
+++ watch-obt.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-#source testenv/bin/activate
+
+if [ ! -d testenv ] ; then
+    virtualenv --python=python3.4 testenv
+    source testenv/bin/activate
+    pip3 install -r requirements.txt
+    deactivate
+fi
+
+source testenv/bin/activate
 ./watch.py ~/PhpstormProjects/pronq/obt_prototypes/ -u root -p vagrant -s localdev.saas.hp.com \
     --map "server/cms:/var/www/drupal" \
     --map "shared-assets:/var/www/drupal/sites/all/themes/zen_pronq_mktg/shared-assets" \
